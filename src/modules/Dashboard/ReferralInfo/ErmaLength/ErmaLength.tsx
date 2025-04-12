037c6db06df1f5fc57df1b1c4d7309a128fafa9c
yevhenii.drahancha1 <yevhenii.drahancha@gmail.com>
2025-04-12 14:46:06 +0200

    merged main

--- src/modules/Dashboard/ReferralInfo/ErmaLength/ErmaLength.tsx
+++ src/modules/Dashboard/ReferralInfo/ErmaLength/ErmaLength.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import CopyIcon from "../../../../assets/copy.svg";
-import SmileIcon from ;
+import SmileIcon from "../../../../assets/smile.svg";
 
 import "./styles.scss";
 
@@ -10,7 +10,7 @@ const ErmaLength = () => {
   return (
     <div className={baseClassName}>
       <div className={`${baseClassName}__imgWrapper`}>
-        <Image  src={}/>
+        <Image src={SmileIcon} alt="smile icon" width={48} height={48} />
       </div>
       <div className={`${baseClassName}__ermaLengthWrapper`}>
         <p className={`${baseClassName}__ermaLengthTitle`}> ermalength</p>
@@ -18,7 +18,7 @@ const ErmaLength = () => {
           <p className={`${baseClassName}__link`}>
             https://hehe.com/ref=$9295xj-3928x03pox039jfh930_039x893
           </p>
-          <CopyIcon />
+          <Image src={CopyIcon} alt="copy" width={16} height={16} />
           <p className={`${baseClassName}__ermaLengthText`}>
             Invited 01.05.2024 by{" "}
             <span className={`${baseClassName}__ermaLengthId`}>ID 2411</span>
