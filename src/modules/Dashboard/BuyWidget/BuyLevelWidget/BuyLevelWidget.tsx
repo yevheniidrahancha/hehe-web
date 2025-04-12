ae2ce25e5188371734948a097c6e6995f648d62d
yevheniidrahancha <yevhenii.drahancha@gmail.com>
2025-04-12 20:19:53 +0200

    Buy Level

--- src/modules/Dashboard/BuyWidget/BuyLevelWidget/BuyLevelWidget.tsx
+++ src/modules/Dashboard/BuyWidget/BuyLevelWidget/BuyLevelWidget.tsx
@@ -1,9 +1,47 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
+import EthereumIcon from "../../../../assets/ethereum.svg";
+import ChevronIcon from "../../../../assets/chevron-down.svg";
+
 import "./styles.scss";
 
+import {
+  colors,
+  FormControl,
+  InputBase,
+  InputLabel,
+  MenuItem,
+  Select,
+  styled,
+} from "@mui/material";
+
 const baseClassName = "buy-level-widget";
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(3),
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 8,
+    position: "relative",
+    backgroundColor: "#252735",
+    fontSize: 16,
+    width: 360,
+    padding: "10px 26px 10px 12px",
+    color: "#FFF",
+    fontFamily: '"SF Pro Display", sans-serif',
+    fontWeight: 500,
+    lineHeight: "20px",
+
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}));
 
 const BuyLevelWidget = () => {
   const [selectLevel, setSelectLevel] = useState("");
@@ -19,54 +57,59 @@ const BuyLevelWidget = () => {
   return (
     <div className={baseClassName}>
       <div className={`${baseClassName}__dropdowns-wrapper`}>
-        <select
-          value={selectLevel}
-          onChange={handleSelectLevel}
-          className={`${baseClassName}__dropdowns-wrapper`}
-        >
-          <option value="">Select an option</option>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
+        <FormControl sx={{ m: 1 }} variant="standard">
+          <Select
+            labelId="demo-customized-select-label"
+            id="demo-customized-select"
+            value={selectLevel}
+            onChange={handleSelectLevel}
+            input={<BootstrapInput />}
+            displayEmpty
+            sx={{
+              "& .MuiSelect-icon": {
+                color: "#FFF",
+              },
+            }}
+          >
+            <MenuItem
+              sx={{
+                fontFamily: '"SF Pro Display", sans-serif',
+                fontWeight: 500,
+                fontStyle: "unset",
+              }}
+              value=""
+              disabled
+            >
+              Select Level
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
 
-        <select
-          value={selectСurrency}
-          onChange={handleSelectCurrency}
-          style={{
-            padding: "5px 8px",
-            border: "1px solid #b0b4c6",
-            borderRadius: "4px",
-            fontSize: "14px",
-            width: "200px",
-          }}
-        >
-          <option value="">Select an image option</option>
-          <option value="image1">
-            <img
-              src="https://via.placeholder.com/20"
-              alt="icon1"
-              style={{ width: "20px", marginRight: "10px" }}
-            />
-            Image 1
-          </option>
-          <option value="image2">
-            <img
-              src="https://via.placeholder.com/20"
-              alt="icon2"
-              style={{ width: "20px", marginRight: "10px" }}
-            />
-            Image 2
-          </option>
-          <option value="image3">
-            <img
-              src="https://via.placeholder.com/20"
-              alt="icon3"
-              style={{ width: "20px", marginRight: "10px" }}
-            />
-            Image 3
-          </option>
-        </select>
+        <FormControl>
+          <InputLabel>Currency</InputLabel>
+          <Select
+            value={selectСurrency}
+            onChange={handleSelectCurrency}
+            label="Currency"
+            className={`${baseClassName}__dropdown-currency`}
+          >
+            <MenuItem value="ETH">
+              <Image src={EthereumIcon} alt="Ethereum" width={20} height={20} />
+              ETH
+            </MenuItem>
+            <MenuItem value="ETH">
+              <Image src={EthereumIcon} alt="Ethereum" width={20} height={20} />
+              ETH
+            </MenuItem>
+            <MenuItem value="ETH">
+              <Image src={EthereumIcon} alt="Ethereum" width={20} height={20} />
+              ETH
+            </MenuItem>
+          </Select>
+        </FormControl>
       </div>
     </div>
   );
