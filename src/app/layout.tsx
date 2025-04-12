dab22ffac7fa05f01e55e65529b00ec44ee08ee7
Yevhenii Drahancha <yevhenii.drahancha@gmail.com>
2025-04-12 14:09:52 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./styles.scss";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="layout">{children}</div>
       </body>
     </html>
   );
